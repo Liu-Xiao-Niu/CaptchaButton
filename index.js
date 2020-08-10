119d22ce7a6fb5323b685eaf1db2e79dd668188a
liuwenxin <291429687@qq.com>
2020-08-10 22:43:30 +0800

    添加文件

---
+++ index.js
@@ -0,0 +1,39 @@
+ const Counter = {
+	counting: false,
+	disabled: false,
+	btnText: '获取验证码',
+	timeSlot: 10,
+	countDown: function(obj) {
+
+		if (this.disabled) {
+			return;
+		}
+
+		let timer, s = this.timeSlot
+
+		this.disabled = true
+		obj.innerHTML = s + '秒后重发'
+		this.counting = true
+
+		timer = setInterval(() => {
+
+			if (this.counting) {
+				if (s > 0) {
+					obj.innerHTML = (--s) + '秒后重发';
+				} else {
+
+					clearInterval(timer)
+					obj.innerHTML = '重新获取'
+					this.counting = false
+					this.disabled = false
+
+				}
+			} else {
+				clearInterval(timer)
+				obj.innerHTML = this.defaultText
+				this.disabled = false
+			}
+
+		}, 1000);
+	}
+}
