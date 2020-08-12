5de861581fdc452ea8134b3ecb2e63afbbc03afc
liuwenxin <291429687@qq.com>
2020-08-12 16:39:07 +0800

    添加vue组件

---
+++ vue/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+	  <div class="box">
+		<div>
+		  <p>
+			  <label>手机号：</label>
+			  <input v-model="account" placeholder="请输入手机号" />
+		  </p>
+		  <p style="display: flex;">
+			  <label>验证码：</label>
+			  <input placeholder="请输入验证码" style="margin-right: 20px;"/>
+			  
+			  <CaptchaButton 
+				ref="captcha"
+			  />
+			  
+		  </p>
+		  
+		  <p style="text-align: center;">
+			  <button>登录</button> <button @click="cancleCounter">停止倒计时</button>
+		  </p>
+		</div>
+		  
+	  </div>
+	  
+  </div>
+</template>
+
+<script>
+import CaptchaButton from './components/CaptchaButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CaptchaButton
+  },
+  data(){
+	return{
+		account:''
+	}
+  },
+  methods:{
+	  cancleCounter(){
+		  this.$refs.captcha.counting = false
+	  }
+  }
+}
+</script>
+
+<style>
+	.box{
+		display: flex;
+		justify-content: center;	
+	}
+</style>
---
+++ vue/src/components/CaptchaButton.vue
@@ -0,0 +1,100 @@
+<template>
+	<div>
+		<button :disabled="counting" @click="sendCode">
+			{{btnText}}
+		</button>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	
+	var timer;
+	
+	export default {
+		props: {
+			defaultText: {
+				type: String,
+				default: '获取验证码'
+			},
+			timeSlot: {
+				type: Number,
+				default: 60
+			},
+			account: {
+				type: String,
+				default: ''
+			},
+			accountEmptyTips:{
+				type: String,
+				default: '请输入手机号'
+			}
+		},
+		data() {
+			return {
+				counting: false,
+				btnText: ''
+			};
+		},
+		methods: {
+			sendCode:function(){
+				
+				if(this.account == ""){
+					
+					alert(this.accountEmptyTips)
+					
+				}else if(!this.counting){
+					
+					let url = '',param = {}
+					
+					axios.post(url,param).then((res) => { //发送验证码
+
+						this.countDown() //发送成功开始倒计时
+						
+					})
+				}
+			},
+			countDown: function() {
+
+				if (this.counting) {
+					return;
+				}
+
+				let s = this.timeSlot,sAfterTxt = '秒后重发'
+
+			
+				this.btnText = s + sAfterTxt
+				this.counting = true
+
+				timer = setInterval(() => {
+
+					if (this.counting) {
+						if (s > 0) {
+							this.btnText = (--s) + sAfterTxt
+						} else {
+							this.counting = false
+							this.stopTimer('重新获取')
+						}
+					} else { //手动停止倒计时
+						this.stopTimer(this.defaultText)
+					}
+
+				}, 1000);
+			},
+			stopTimer: function(txt) {
+				this.btnText = txt
+				clearInterval(timer)
+				
+			}
+		},
+		created() {
+			this.btnText = this.defaultText
+		},
+		destroyed() {
+			this.counting = false
+		}
+	}
+</script>
+
+<style>
+</style>
